7476df427005d808b64ffa7332065ff6a375169c
ridhamjeetkaur <ridhamjeetkaur.29@gmail.com>
2025-08-03 14:50:58 +0530

    api  fetch in deployment

--- client/src/components/MainContent.tsx
+++ client/src/components/MainContent.tsx
@@ -19,7 +19,7 @@ export type Book = {
 
 
 // Then, in your JavaScript file, you can reference it like this:
-const API_BASE_URL = process.env.NODE_ENV === 'development'
+const API_BASE_URL = process.env.NODE_ENV === 'production'
   ? '/api' // Empty for production (same domain)
   : process.env.REACT_APP_API_BASE_URL; // Use the variable from your .env file
 
